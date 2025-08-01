eb8c2172dff5c67d5d498f037121571cc30e1a90
altairFonAlfheim <insania_officialis@outlook.com>
2025-08-01 17:11:17 +0300

    -Доработка инициализации координат -Исправление ошибки с контекстом базы данных

---
+++ Insania.Geography.Database/Migrations/20250801140038_EditStartIds.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Insania.Geography.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditStartIds : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "id",
+                schema: "insania_geography",
+                table: "u_geography_objects_coordinates",
+                type: "bigint",
+                nullable: false,
+                comment: "Первичный ключ таблицы",
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldComment: "Первичный ключ таблицы")
+                .Annotation("Npgsql:IdentitySequenceOptions", "'41', '1', '', '', 'False', '1'")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn)
+                .OldAnnotation("Npgsql:IdentitySequenceOptions", "'5', '1', '', '', 'False', '1'")
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "id",
+                schema: "insania_geography",
+                table: "r_coordinates",
+                type: "bigint",
+                nullable: false,
+                comment: "Первичный ключ таблицы",
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldComment: "Первичный ключ таблицы")
+                .Annotation("Npgsql:IdentitySequenceOptions", "'79', '1', '', '', 'False', '1'")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn)
+                .OldAnnotation("Npgsql:IdentitySequenceOptions", "'5', '1', '', '', 'False', '1'")
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "id",
+                schema: "insania_geography",
+                table: "u_geography_objects_coordinates",
+                type: "bigint",
+                nullable: false,
+                comment: "Первичный ключ таблицы",
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldComment: "Первичный ключ таблицы")
+                .Annotation("Npgsql:IdentitySequenceOptions", "'5', '1', '', '', 'False', '1'")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn)
+                .OldAnnotation("Npgsql:IdentitySequenceOptions", "'41', '1', '', '', 'False', '1'")
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "id",
+                schema: "insania_geography",
+                table: "r_coordinates",
+                type: "bigint",
+                nullable: false,
+                comment: "Первичный ключ таблицы",
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldComment: "Первичный ключ таблицы")
+                .Annotation("Npgsql:IdentitySequenceOptions", "'5', '1', '', '', 'False', '1'")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn)
+                .OldAnnotation("Npgsql:IdentitySequenceOptions", "'79', '1', '', '', 'False', '1'")
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+        }
+    }
+}
