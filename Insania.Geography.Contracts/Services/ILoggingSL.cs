0fc9bf3d1828b0aca572c30fa31a9b3256f47513
Альтаир фон Альфхейм <insania_officialis@outlook.com>
2025-10-02 08:45:46 +0300

    - Добавление фоновой записи логов в бд

---
+++ Insania.Geography.Contracts/Services/ILoggingSL.cs
@@ -0,0 +1,16 @@
+﻿using Insania.Geography.Entities;
+
+namespace Insania.Geography.Contracts.Services;
+
+/// <summary>
+/// Интерфейс сервиса фонового логгирования в бд
+/// </summary>
+public interface ILoggingSL
+{
+    /// <summary>
+    /// Метод постановки лога в очередь на обработку
+    /// </summary>
+    /// <param cref="LogApiGeography" name="log">Лог для записи</param>
+    /// <returns cref="ValueTask">Задание</returns>
+    ValueTask QueueLogAsync(LogApiGeography log);
+}
--- Insania.Geography.Middleware/LoggingMiddleware.cs
+++ Insania.Geography.Middleware/LoggingMiddleware.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 
 using Newtonsoft.Json;
 
+using Insania.Shared.Messages;
 using Insania.Shared.Models.Requests.Logs;
 
+using Insania.Geography.Contracts.Services;
 using Insania.Geography.Database.Contexts;
 using Insania.Geography.Entities;
 
@@ -12,14 +15,20 @@ namespace Insania.Geography.Middleware;
 /// <summary>
 /// Конвейер запросов логгирования
 /// </summary>
-/// <param name="next">Делегат следующего метода</param>
-public class LoggingMiddleware(RequestDelegate next)
+/// <param cref="RequestDelegate" name="next">Делегат следующего метода</param>
+/// <param cref="ILogger{LoggingMiddleware}" name="logger">Сервис логгирования</param>
+public class LoggingMiddleware(RequestDelegate next, ILogger<LoggingMiddleware> logger)
 {
     #region Зависимости
     /// <summary>
     /// Делегат следующего метода
     /// </summary>
     private readonly RequestDelegate _next = next;
+
+    /// <summary>
+    /// Сервис логгирования
+    /// </summary>
+    private readonly ILogger<LoggingMiddleware> _logger = logger;
     #endregion
 
     #region Поля
@@ -38,9 +47,9 @@ public class LoggingMiddleware(RequestDelegate next)
     /// <summary>
     /// Метод перехватывания запросов
     /// </summary>
-    /// <param name="context">Контекст запроса</param>
-    /// <param name="contextDB">Контекст базы данных</param>
-    public async Task Invoke(HttpContext context, LogsApiGeographyContext contextDB)
+    /// <param cref="HttpContext" name="context">Контекст запроса</param>
+    /// <param cref="ILoggingSL" name="loggingSL">Сервис логгирования в бд</param>
+    public async Task Invoke(HttpContext context, ILoggingSL loggingSL)
     {
         //Получение параметров запроса
         var method = context.Request.Path; //адрес запроса
@@ -49,53 +58,81 @@ public class LoggingMiddleware(RequestDelegate next)
 
         //Запись в базу данных начала выполнения
         LogApiGeography log = new("system", true, method, type, request, null);
-        contextDB.Logs.Add(log);
-        await contextDB.SaveChangesAsync();
+        _ = Task.Run(async () =>
+        {
+            try
+            {
+                await loggingSL.QueueLogAsync(log);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ErrorMessages.Error);
+            }
+        });
 
         //Объявление переменной ответа
         string? response = null;
 
-        //Проверка исключений
-        if (!_exceptions.Any(x => method.ToString().Contains(x)))
+        try
         {
-            //Получение оригинального потока ответа
-            var originalBodyStream = context.Response.Body;
+            //Проверка исключений
+            if (!_exceptions.Any(x => method.ToString().Contains(x)))
+            {
+                //Получение оригинального потока ответа
+                var originalBodyStream = context.Response.Body;
 
-            //Перехват тела ответа
-            using var responseBody = new MemoryStream();
-            context.Response.Body = responseBody;
-            await _next(context);
-            response = await GetResponse(context.Response);
+                //Перехват тела ответа
+                using var responseBody = new MemoryStream();
+                context.Response.Body = responseBody;
+                await _next(context);
+                response = await GetResponse(context.Response);
 
-            //Получение кода статуса
-            var statusCode = context.Response.StatusCode;
+                //Получение кода статуса
+                var statusCode = context.Response.StatusCode;
 
-            //Определение успешности ответа
-            var success = _successCodes.Any(x => x == statusCode);
+                //Определение успешности ответа
+                var success = _successCodes.Any(x => x == statusCode);
 
-            //Запись результата выполнения в лог
-            log.SetEnd(success, response, statusCode);
-            contextDB.Logs.Update(log);
-            await contextDB.SaveChangesAsync();
+                //Запись результата выполнения в лог
+                log.SetEnd(success, response, statusCode);
 
-            //Возвращение в ответ оригинального потока
-            await responseBody.CopyToAsync(originalBodyStream);
+                //Возвращение в ответ оригинального потока
+                await responseBody.CopyToAsync(originalBodyStream);
+            }
+            else
+            {
+                //Переход к следующему элементу
+                await _next(context);
+
+                //Получение кода статуса
+                var statusCode = context.Response.StatusCode;
+
+                //Определение успешности ответа
+                var success = _successCodes.Any(x => x == statusCode);
+
+                //Запись результата выполнения в лог
+                log.SetEnd(success, response, statusCode);
+            }
         }
-        else
+        catch (Exception ex)
         {
-            //Переход к следующему элементу
-            await _next(context);
-
-            //Получение кода статуса
-            var statusCode = context.Response.StatusCode;
-
-            //Определение успешности ответа
-            var success = _successCodes.Any(x => x == statusCode);
-
-            //Запись результата выполнения в лог
-            log.SetEnd(success, response, statusCode);
-            contextDB.Logs.Update(log);
-            await contextDB.SaveChangesAsync();
+            //Логгирование ошибки
+            log.SetEnd(false, ex.Message, 500);
+        }
+        finally
+        {
+            //Запись в бд окончания выполнения
+            _ = Task.Run(async () =>
+            {
+                try
+                {
+                    await loggingSL.QueueLogAsync(log);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, ErrorMessages.Error);
+                }
+            });
         }
     }
     #endregion
